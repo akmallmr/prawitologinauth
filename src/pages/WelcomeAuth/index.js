b77af2084af706ccbc74e4132b9bb35690d32d45
akmallmr <56009089+akmallmr@users.noreply.github.com>
2020-04-13 16:31:39 +0700

    Structure Programs Real App

--- src/pages/WelcomeAuth/index.js
+++ src/pages/WelcomeAuth/index.js
@@ -1,12 +1,47 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {View, Text} from 'react-native';
+
+import ActionButton from './ActionButton';
+import { colors } from '../../utils';
 
 const WelcomeAuth = () => {
-    return(
-        <View>
-            <Text> Welcome Auth Page </Text>
-        </View>
-    )
-}
+  return (
+    <View style={styles.wrapper.page}>
+      <View style={styles.wrapper.illustration}/>
+      <Text style={styles.text.welcome}>Selamat Datang di O - JOL</Text>
+      <ActionButton
+        desc="Silahkan Masuk, jika anda sudah memiliki akun"
+        title="Masuk"
+      />
+      <ActionButton desc="atau
+      silahkan daftar jika anda belum memiliki akun," title="Daftar" />
+    </View>
+  );
+};
+
+const styles = {
+  wrapper: {
+    page: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: 'white',
+    },
+    illustration: {
+      width: 219,
+      height: 117,
+      backgroundColor: colors.default,
+      marginBottom: 10,
+    },
+  },
+  text: {
+    welcome: {
+      fontSize: 18,
+      color: colors.default,
+      fontWeight: 'bold',
+      marginBottom: 76,
+    },
+  },
+};
 
 export default WelcomeAuth;
