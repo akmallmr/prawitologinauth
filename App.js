3151ee5132d4a25bbf3cf8fb190e765d47b0bd8d
akmallmr <56009089+akmallmr@users.noreply.github.com>
2020-04-13 12:26:17 +0700

    Container

--- App.js
+++ App.js
@@ -18,7 +18,9 @@ const App = () => {
 
 const styles = StyleSheet.create ({
   container: {
-    flex: 1
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 })
 export default App;
